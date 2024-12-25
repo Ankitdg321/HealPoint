9627dad88aab66fb1c7228f121544b67191c95f4
Ankit Gaikwad <ankitdg321@gmail.com>
2024-12-25 16:54:40 +0530

    Initial commit

---
+++ src/main/java/com/anweb/Mapp/MedCentrallApplication.java
@@ -0,0 +1,13 @@
+package com.anweb.Mapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedCentrallApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedCentrallApplication.class, args);
+	}
+
+}
